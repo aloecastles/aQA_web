44cef344ad7d9e55241f39a9efc5edfa2e97b521
Alexey <o.rohalskiy@gmail.com>
2022-01-25 23:55:15 +0200

    ExtractedMethods (with working login and pause asset)

--- test_123.py
+++ test_123.py
@@ -20,34 +20,75 @@ class test1(unittest.TestCase):
     def test_test1(self):
         success = True
         wd = self.wd
-        #app open
-        wd.get("https://tv.kyivstar.ua")
-        wd.maximize_window()
-        #search button click
-        time.sleep(3)
-        wd.find_element(By.CSS_SELECTOR, "body > vd-root > vd-navbar > div.nav > div.nav__btn.nav__search > button").click()
-        #search query
-        wd.find_element(By.CSS_SELECTOR, "#cdk-overlay-0 > vd-sidenav-search > div > div.header > form > div > input").send_keys("суперсила")
-        #open first search result in 1st row
-        time.sleep(3)
-        wd.find_element(By.CSS_SELECTOR, "#cdk-overlay-0 > vd-sidenav-search > div > div:nth-child(3) > div > div > vd-search-tiles > div > div.tiles-wrapper > div > img").click()
-        #opened VOD page big play button click
-        wd.find_element(By.CSS_SELECTOR, "body > vd-root > div > vd-details-page > vd-vod > div.asset.ng-star-inserted > div > div > div.actions > button").click()
-        #login page personal account button click
-        wd.find_element(By.CSS_SELECTOR, "#cdk-overlay-1 > vd-login-rightdrawer > div > div > vd-login-rightdrawer-form > vd-tabs > div > div:nth-child(2)").click()
-        # login page "Email or personal account number" field type in
-        wd.find_element(By.CSS_SELECTOR, "#cdk-overlay-1 > vd-login-rightdrawer > div > div > vd-login-rightdrawer-form > vd-tabs > section > div > vd-login-contract-form > div > form > vd-form-field:nth-child(1) > div > div.vd-form-field__field-wrap > input").send_keys("demo_autotest")
-        # login page "Password" field type in
-        wd.find_element(By.CLASS_NAME, "login-input_password").send_keys("123456")
-        # login page "login" button click
-        wd.find_element(By.CSS_SELECTOR, "#cdk-overlay-1 > vd-login-rightdrawer > div > div > vd-login-rightdrawer-form > vd-tabs > section > div > vd-login-contract-form > div > form > button.btn.btn-primary.btn_login.next-step").click()
-        # opened VOD page volume off in player
-        wd.find_element(By.CLASS_NAME, "icon-volume").click()
+        self.applicationLaunch(wd)
+        self.searchOpen(wd)
+        self.searchQuery(wd)
+        self.firstSearchResultOpen(wd)
+        self.VODassetPageBigPlayClick(wd)
+        self.loginPagePA_Click(wd)
+        self.loginPagePA_emailFieldType(wd)
+        self.loginPagePA_passwordType(wd)
+        self.loginPagePA_loginBittonClick(wd)
+        self.VODplayerVolumeOff(wd)
+        self.VODplayerPauseClick(wd)
+        time.sleep(5)
+
+    def VODplayerPauseClick(self, wd):
         # opened VOD page pause(\play) click
         time.sleep(1)
         wd.find_element(By.CLASS_NAME, "btn-play").click()
-        time.sleep(5)
-        self.assertTrue(success)
+
+    def VODplayerVolumeOff(self, wd):
+        # opened VOD page volume off in player
+        wd.find_element(By.CLASS_NAME, "icon-volume").click()
+
+    def loginPagePA_loginBittonClick(self, wd):
+        # login page "login" button click
+        wd.find_element(By.CSS_SELECTOR,
+                        "#cdk-overlay-1 > vd-login-rightdrawer > div > div > vd-login-rightdrawer-form > vd-tabs > section > div > vd-login-contract-form > div > form > button.btn.btn-primary.btn_login.next-step").click()
+
+    def loginPagePA_passwordType(self, wd):
+        # login page "Password" field type in
+        wd.find_element(By.CLASS_NAME, "login-input_password").send_keys("123456")
+
+    def loginPagePA_emailFieldType(self, wd):
+        # login page "Email or personal account number" field type in
+        wd.find_element(By.CSS_SELECTOR,
+                        "#cdk-overlay-1 > vd-login-rightdrawer > div > div > vd-login-rightdrawer-form > vd-tabs > section > div > vd-login-contract-form > div > form > vd-form-field:nth-child(1) > div > div.vd-form-field__field-wrap > input").send_keys(
+            "demo_autotest")
+
+    def loginPagePA_Click(self, wd):
+        # login page personal account button click
+        wd.find_element(By.CSS_SELECTOR,
+                        "#cdk-overlay-1 > vd-login-rightdrawer > div > div > vd-login-rightdrawer-form > vd-tabs > div > div:nth-child(2)").click()
+
+    def VODassetPageBigPlayClick(self, wd):
+        # opened VOD page big play button click
+        wd.find_element(By.CSS_SELECTOR,
+                        "body > vd-root > div > vd-details-page > vd-vod > div.asset.ng-star-inserted > div > div > div.actions > button").click()
+
+    def firstSearchResultOpen(self, wd):
+        # open first search result in 1st row
+        time.sleep(3)
+        wd.find_element(By.CSS_SELECTOR,
+                        "#cdk-overlay-0 > vd-sidenav-search > div > div:nth-child(3) > div > div > vd-search-tiles > div > div.tiles-wrapper > div > img").click()
+
+    def searchQuery(self, wd):
+        # search query
+        wd.find_element(By.CSS_SELECTOR,
+                        "#cdk-overlay-0 > vd-sidenav-search > div > div.header > form > div > input").send_keys(
+            "суперсила")
+
+    def searchOpen(self, wd):
+        # search button click
+        time.sleep(3)
+        wd.find_element(By.CSS_SELECTOR,
+                        "body > vd-root > vd-navbar > div.nav > div.nav__btn.nav__search > button").click()
+
+    def applicationLaunch(self, wd):
+        # app open
+        wd.get("https://tv.kyivstar.ua")
+        wd.maximize_window()
 
     def tearDown(self):
         self.wd.quit()
