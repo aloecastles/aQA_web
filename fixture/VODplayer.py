41ae31259f0ffaeadbe001bd7b5c0c393236c3f4
Alexey <o.rohalskiy@gmail.com>
2022-02-02 22:56:55 +0200

    VOD player helper created

---
+++ fixture/VODplayer.py
@@ -0,0 +1,25 @@
+from selenium.webdriver.common.by import By
+import time
+
+class VODplayerHelper:
+    def __init__(self, app):
+        self.app = app
+
+    def VODplayerPauseClick(self):
+        wd = self.app.wd
+        # opened VOD page pause(\play) click
+        time.sleep(1)
+        wd.find_element(By.CLASS_NAME, "btn-play").click()
+
+    def VODplayerVolumeOff(self):
+        wd = self.app.wd
+        # opened VOD page volume off in player
+        wd.find_element(By.CLASS_NAME, "icon-volume").click()
+
+
+
+    def VODassetPageBigPlayClick(self):
+        wd = self.app.wd
+        # opened VOD page big play button click
+        wd.find_element(By.CSS_SELECTOR,
+                        "body > vd-root > div > vd-details-page > vd-vod > div.asset.ng-star-inserted > div > div > div.actions > button").click()
